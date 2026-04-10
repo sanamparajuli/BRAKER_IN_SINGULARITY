02b74b60793baae7831b1ccc3de40540099a6310
Sanam Parajuli <128193197+sanamparajuli@users.noreply.github.com>
2026-04-09 22:14:33 -0500

    Update augustus_setup.sh

--- augustus_setup.sh
+++ augustus_setup.sh
@@ -3,7 +3,7 @@ singularity exec --bind /path/to/rundir:/mnt ./braker3.sif bash
 
 singularity exec braker3.sif bash #this will open the sif image, now copy the files from the image to the mounted dir
 
-Singularity cp -r /opt/Augustus/config /mnt/augustus_config
+>Singularity cp -r /opt/Augustus/config /mnt/augustus_config
 
 #test if it is writable
 touch ./augustus_config/config/species/test #if it creates a new file test, it is writable
